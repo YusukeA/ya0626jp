b52c77b4a730cd331dd8d895d67631966286a777
YusukeA <ya0626jp@gmail.com>
2016-09-07 13:22:02 +0900

    persbox ver.2.0.3 (#13)
    * pers box commited
    
    * pers-box back up
    
    * pers box check
    
    * footer change
    
    * pers box fixed
    
    * pers box ver.1.20
    
    * persbox ver.2.0.2

---
+++ persbox2/js/slide-change.js
@@ -0,0 +1,22 @@
+
+$(function() {
+    var array = [
+    "/persbox/img/pers/pers01.png",
+    "/persbox/img/pers/pers02.png",
+    "/persbox/img/pers/pers03.png",
+    "/persbox/img/pers/pers04.png",
+    "/persbox/img/pers/pers05.png",
+    "/persbox/img/pers/pers06.png",
+    "/persbox/img/pers/pers07.png",
+    "/persbox/img/pers/pers08.png",
+    "/persbox/img/pers/pers09.png",
+    "/persbox/img/pers/pers10.png",
+    "/persbox/img/pers/pers12.png",
+    "/persbox/img/pers/pers13.png"
+    ];
+  
+    var l = array.length;
+    var r = Math.floor(Math.random()*l);
+    var bgimgurl = array[r];
+    $(".key_visual").css('background-image',('url("'+bgimgurl+'")'));
+});
